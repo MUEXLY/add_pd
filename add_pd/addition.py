4ac795bbca07796fa0ded72c78117a104de0bb31
Jacob Jeffries <jwjeffr@node0092.palmetto.clemson.edu>
2024-10-07 16:32:11 -0400

    changed signature of add_defect function

--- add_pd/addition.py
+++ add_pd/addition.py
@@ -1,4 +1,3 @@
-from copy import deepcopy
 from pathlib import Path
 from enum import Enum
 from typing import Callable, Optional
@@ -37,16 +36,13 @@ def select_n_closest(particle_index: int, n: int) -> Callable:
     return wrapper
 
 
-def add_point_defect(pipeline: ovito.pipeline.Pipeline, rmsd_cutoff: float) -> ovito.pipeline.Pipeline:
+def add_point_defect(pipeline: ovito.pipeline.Pipeline, rmsd_cutoff: float) -> None:
 
-    p = deepcopy(pipeline)
-    p.modifiers.append(PolyhedralTemplateMatchingModifier(rmsd_cutoff=rmsd_cutoff))
-    p.modifiers.append(ExpressionSelectionModifier(expression="StructureType==0"))
-    p.modifiers.append(ClusterAnalysisModifier(only_selected=True, sort_by_size=True, compute_com=True))
-    p.modifiers.append(add_particles_in_clusters())
-    p.modifiers.append(WrapPeriodicImagesModifier())
-
-    return p
+    pipeline.modifiers.append(PolyhedralTemplateMatchingModifier(rmsd_cutoff=rmsd_cutoff))
+    pipeline.modifiers.append(ExpressionSelectionModifier(expression="StructureType==0"))
+    pipeline.modifiers.append(ClusterAnalysisModifier(only_selected=True, sort_by_size=True, compute_com=True))
+    pipeline.modifiers.append(add_particles_in_clusters())
+    pipeline.modifiers.append(WrapPeriodicImagesModifier())
 
 
 def new_run(input_path: Path, output_path: Path, defect: DefectType, rmsd_cutoff: float, export_kwargs: Optional[dict] = None) -> None:
@@ -64,7 +60,7 @@ def new_run(input_path: Path, output_path: Path, defect: DefectType, rmsd_cutoff
         }
 
     pipeline = ovito.io.import_file(input_path)
-    pipeline = add_point_defect(pipeline, rmsd_cutoff)
+    add_point_defect(pipeline, rmsd_cutoff)
 
     # need to delete two particles closest to defect if self-interstitial
     if defect == DefectType.VACANCY:
